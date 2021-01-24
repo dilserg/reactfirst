31d06c06a06dec17673ca4d5b7b50ec78cc9fedb
dilserg <i89206154925@gmail.com>
2021-01-25 00:54:47 +0300

    major update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import Header from "./components/Header/Header";
 import './App.css';
+import Menu from "./components/Menu/Menu";
+import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
 
-function App() {
+const App = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header/>
+        <Menu/>
+        <div className="content">
+          <Route path="/profile" render={() => <Profile />} />
+          <Route path="/dialogs" render={() => <Dialogs messages={props.messages} DialogMessages={props.DialogMessages}/>} />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
