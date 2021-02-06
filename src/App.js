8348398fb7d29303e37fb5f4a00bfeacd8eb7c66
dilserg <i89206154925@gmail.com>
2021-02-06 18:19:41 +0300

    refactoring

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
     if (!this.props.isInitialized)
       return <div className='preloader'><Preloader/></div>;
     return (
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <HeaderContainer/>
         <div className='App'>
           <MenuContainer/>
